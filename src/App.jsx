6638d048fd50b0bc8e45b31cc85790e80f7983f0
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-06-04 19:04:46 +0530

    fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import Tours from './pages/Tours';
 import Events from './pages/Events';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
-import TourPage from './pages/TourPages';
+import IncredibleOdishaTour from './pages/tourpages/IncredibleOdisha';
+import NorthBengalTour from './pages/tourpages/NorthBengalSikkim';
+import InternationalTours from './pages/tourpages/InternationalTours';
+import IslandOfIndia from './pages/tourpages/IslandOfIndia';
+import NorthIndiaTours from './pages/tourpages/NorthIndiaTours';
+import SouthIndiaTours from './pages/tourpages/SouthIndiaTours';
+import WesternIndiaTours from './pages/tourpages/WesternIndiaTours';
 
 function App() {
   return (
@@ -19,7 +25,13 @@ function App() {
         <Route path="/events" element={<Events />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/tour/:tourSlug" element={<TourPage />} />
+        <Route path="/tour/incredible-odisha" element={<IncredibleOdishaTour />} />
+        <Route path="/tour/north-bengal-sikkim" element={<NorthBengalTour />} />
+        <Route path="/tour/international" element={<InternationalTours />} />
+        <Route path="/tour/island-of-india" element={<IslandOfIndia />} />
+        <Route path="/tour/north-india" element={<NorthIndiaTours />} />
+        <Route path="/tour/south-india" element={<SouthIndiaTours />} />
+        <Route path="/tour/western-india" element={<WesternIndiaTours />} />
       </Routes>
     </Router>
   );
