bab18f3ad676d52353b751cd9935806cae35cfdc
INZAMAMULHAQUE-0 <ihaque3040@gmail.com>
2025-06-07 01:24:33 +0530

    chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ import AdventureTours from './pages/tourpages/AdventureTours';
 import NorthIndiaPackage from './pages/NorthIndiaTours';
 import WesternIndiaPackage from './pages/WesternIndiaTours';
 import SouthIndiaTrips from './pages/SouthIndiaTours';
+import Chatbot from './components/Chatbot';
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
       <ScrollToTop />
       <Navbar />
       <Routes>
+        <Route path='/chatbot' element={<Chatbot/>}/>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/tours" element={<Tours />} />
